42c5cbe6ba3ac65d33bc4d41a9deafbca23b4a05
Leon Rausch <rauschleon022@gmail.com>
2022-02-04 20:14:13 +0100

    added FloatingActionButton with onclick function

--- app/src/main/java/com/example/mysmartnutrition/MainActivity.java
+++ app/src/main/java/com/example/mysmartnutrition/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.mysmartnutrition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Objects from the activity_main.xml file
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddProduct.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
