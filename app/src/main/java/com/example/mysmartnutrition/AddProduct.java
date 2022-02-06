2cb57c93c6678354dbbe4a6a577f3f3f6b3f42a5
Leon Rausch <rauschleon022@gmail.com>
2022-02-06 17:17:35 +0100

    added Button for QR-Code-Scanner later

--- app/src/main/java/com/example/mysmartnutrition/AddProduct.java
+++ app/src/main/java/com/example/mysmartnutrition/AddProduct.java
@@ -2,13 +2,28 @@ package com.example.mysmartnutrition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddProduct extends AppCompatActivity {
+
+    Button QRCodeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
+
+        QRCodeButton = findViewById(R.id.qr_code_button);
+
+        QRCodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                startActivity(intent);
+            }
+        });
     }
 }
