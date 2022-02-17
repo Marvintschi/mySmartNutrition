1eeda882e5df91a0beba558697f0bc03a5201b01
Leon Rausch <rauschleon022@gmail.com>
2022-02-17 22:49:11 +0100

    random message

---
+++ app/src/main/java/com/example/mysmartnutrition/AddProductDetails.java
@@ -0,0 +1,15 @@
+package com.example.mysmartnutrition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddProductDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product_details);
+    }
+
+}
