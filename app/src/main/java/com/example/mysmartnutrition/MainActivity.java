2ace064f4c00bfdc27ceb13cd33712d84d153823
Leon Rausch <rauschleon022@gmail.com>
2022-02-26 23:46:13 +0100

    Step Counter implemented (not working yet) and design changes for barcode scanner

--- app/src/main/java/com/example/mysmartnutrition/MainActivity.java
+++ app/src/main/java/com/example/mysmartnutrition/MainActivity.java
@@ -2,30 +2,48 @@ package com.example.mysmartnutrition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     // Objects from the activity_main.xml file
-    FloatingActionButton addButton;
-    Button addBreakfast;
-    Button addLunch;
-    Button addDinner;
+    private FloatingActionButton addButton;
+    private Button addBreakfast;
+    private Button addLunch;
+    private Button addDinner;
+    private TextView tvStepCounter;
+
+    private SensorManager sensorManager;
+    private Sensor stepCounterSensor;
+    private boolean isStepCounterPresent;
+    int stepCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
         addButton = findViewById(R.id.fab);
         addBreakfast = findViewById(R.id.add_product_breakfast);
         addLunch = findViewById(R.id.add_product_lunch);
         addDinner = findViewById(R.id.add_product_dinner);
+        tvStepCounter = findViewById(R.id.stepCounter);
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,5 +76,42 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
+            stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            isStepCounterPresent = true;
+        } else {
+            tvStepCounter.setText("Step counter sensor is not present");
+            isStepCounterPresent = false;
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor == stepCounterSensor) {
+            stepCount = (int) event.values[0];
+            tvStepCounter.setText(String.valueOf(stepCount));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
+            sensorManager.registerListener(this, stepCounterSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
+            sensorManager.unregisterListener(this, stepCounterSensor);
+        }
     }
 }
