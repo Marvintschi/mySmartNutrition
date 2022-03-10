560beb584b9579b6b89b654b84919afd9622fa2b
LeonR <rauschleon022@gmail.com>
2022-03-10 08:58:32 +0100

    resetting step counter by date with SharedPreference

--- app/src/main/java/com/example/mysmartnutrition/MainActivity.java
+++ app/src/main/java/com/example/mysmartnutrition/MainActivity.java
@@ -29,6 +29,8 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -170,6 +172,116 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public String savedDate;
     public static String notSavedDate;
 
+    public SharedPreferences sharedPreferences = new SharedPreferences() {
+        @Override
+        public Map<String, ?> getAll() {
+            return null;
+        }
+
+        @Nullable
+        @Override
+        public String getString(String key, @Nullable String defValue) {
+            return null;
+        }
+
+        @Nullable
+        @Override
+        public Set<String> getStringSet(String key, @Nullable Set<String> defValues) {
+            return null;
+        }
+
+        @Override
+        public int getInt(String key, int defValue) {
+            return 0;
+        }
+
+        @Override
+        public long getLong(String key, long defValue) {
+            return 0;
+        }
+
+        @Override
+        public float getFloat(String key, float defValue) {
+            return 0;
+        }
+
+        @Override
+        public boolean getBoolean(String key, boolean defValue) {
+            return false;
+        }
+
+        @Override
+        public boolean contains(String key) {
+            return false;
+        }
+
+        @Override
+        public Editor edit() {
+            return null;
+        }
+
+        @Override
+        public void registerOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener) {
+
+        }
+
+        @Override
+        public void unregisterOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener) {
+
+        }
+    };
+    public SharedPreferences.Editor editor = new SharedPreferences.Editor() {
+        @Override
+        public SharedPreferences.Editor putString(String key, @Nullable String value) {
+            return null;
+        }
+
+        @Override
+        public SharedPreferences.Editor putStringSet(String key, @Nullable Set<String> values) {
+            return null;
+        }
+
+        @Override
+        public SharedPreferences.Editor putInt(String key, int value) {
+            return null;
+        }
+
+        @Override
+        public SharedPreferences.Editor putLong(String key, long value) {
+            return null;
+        }
+
+        @Override
+        public SharedPreferences.Editor putFloat(String key, float value) {
+            return null;
+        }
+
+        @Override
+        public SharedPreferences.Editor putBoolean(String key, boolean value) {
+            return null;
+        }
+
+        @Override
+        public SharedPreferences.Editor remove(String key) {
+            return null;
+        }
+
+        @Override
+        public SharedPreferences.Editor clear() {
+            return null;
+        }
+
+        @Override
+        public boolean commit() {
+            return false;
+        }
+
+        @Override
+        public void apply() {
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -243,13 +355,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             stepCount = (int) event.values[0];
             stepCount -= stepsOfToday;
             savedDate = String.valueOf(java.time.LocalDate.now());
-            // notSavedDate = sharedPreferences.getString("notSavedDate", notSavedDate);
+            notSavedDate = sharedPreferences.getString("notSavedDate", notSavedDate);
             if(!savedDate.equalsIgnoreCase(notSavedDate)) {
                 resetSteps();
                 notSavedDate = savedDate;
                 // adds variable to SharedPreference
-                // editor.putString("notSavedDate", notSavedDate);
-                // editor.commit();
+                editor.putString("notSavedDate", notSavedDate);
+                editor.commit();
             }
             tvStepCounter.setText(String.valueOf(stepCount));
         }
