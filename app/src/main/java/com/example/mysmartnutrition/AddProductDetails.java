f2b52e95dbad924f25063c28e3130d123b48f98c
Leon Rausch <rauschleon022@gmail.com>
2022-03-25 19:20:30 +0100

    small design changes

--- app/src/main/java/com/example/mysmartnutrition/AddProductDetails.java
+++ app/src/main/java/com/example/mysmartnutrition/AddProductDetails.java
@@ -76,10 +76,10 @@ public class AddProductDetails extends AppCompatActivity {
 
         // Create an ArrayAdapter using the string array and a default spinner
         ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter.createFromResource(this, R.array.Mahlzeiten,
-                android.R.layout.simple_spinner_item);
+                R.layout.spinner_item);
 
         // Specify the layout to use when the list of choices appears
-        staticAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        staticAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
 
         // Apply the adapter to the spinner
         staticSpinner.setAdapter(staticAdapter);
@@ -89,7 +89,7 @@ public class AddProductDetails extends AppCompatActivity {
         String[] items = new String[] { "Frühstück", "Mittagessen", "Abendessen", "Snacks" };
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, items);
+                R.layout.spinner_item, items);
 
         mahlzeitAngabe.setAdapter(adapter);
 
