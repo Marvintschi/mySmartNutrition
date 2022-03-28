64b981a14cf7ecb53d942ac4070b5b11d30d1a4e
Leon Rausch <rauschleon022@gmail.com>
2022-03-28 18:10:18 +0200

    Button on CalenderDateSelect added

--- app/src/main/java/com/example/mysmartnutrition/CalenderDateSelect.java
+++ app/src/main/java/com/example/mysmartnutrition/CalenderDateSelect.java
@@ -3,11 +3,16 @@ package com.example.mysmartnutrition;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 
 public class CalenderDateSelect extends AppCompatActivity {
 
-    CalendarView calendarView;
+    private CalendarView calendarView;
+    private Button bestätigen;
+
+    private String selectedDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,17 @@ public class CalenderDateSelect extends AppCompatActivity {
 
         calendarView = findViewById(R.id.calendarView);
 
+        bestätigen = findViewById(R.id.datum_bestätigen);
+
+        bestätigen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedDate = String.valueOf(calendarView.getDate());
+
+                // TODO Fertigstellen
+
+                finish();
+            }
+        });
     }
 }
